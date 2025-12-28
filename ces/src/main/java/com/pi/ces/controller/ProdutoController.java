f8b67f615c92e4add02b68b18692b99c4a1f0baa
minoruzxc <minoruzxc@gmail.com>
2025-12-28 10:23:06 -0300

    paginas template e index adicionadas

--- ces/src/main/java/com/pi/ces/controller/ProdutoController.java
+++ ces/src/main/java/com/pi/ces/controller/ProdutoController.java
@@ -1,11 +1,31 @@
 
 package com.pi.ces.controller;
 
+import com.pi.ces.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping
 public class ProdutoController {
+    
+    @Autowired
+    ProdutoService produtoService;
+    
+    @GetMapping("/")
+    public String root(Model model) {
+        return "index";
+    }
+    
+    @GetMapping("/adicionarproduto")
+    public String adicionarProduto(Model model){
+        return "adicionarproduto";
+    }
+    
+    @GetMapping("/retirarproduto")
+    public String retirarProduto(Model model){
+        return "retirarproduto";
+    }
     
 }
